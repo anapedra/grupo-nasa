68759f92e9f8f86c85587af127f78197f2217e51
anapedra <anapedra.mil@gmail>
2024-08-17 15:46:18 -0300

    Projeto criado

---
+++ versionamento-git/src/main/java/org/anapedra/Main.java
@@ -0,0 +1,9 @@
+package org.anapedra;
+
+public class
+Main {
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt und hallo Git/Github!" +
+                " „Ich bin hier! Wie geht es dir?");
+    }
+}
